3b49f494d342f95503d9a850ce7e0ee38be09126
bhatte1 <vinayak.bhatte@gmail.com>
2023-08-07 19:47:16 -0400

    mvn addition

--- TestSession/src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ TestSession/src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -13,15 +13,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import com.qa.opencart.frameworkexception.FrameException;
 
 public class DriverFactory {
-	
+
 	WebDriver driver;
-	
-	public WebDriver initDriver(Properties prop)   //call by reference
+
+	public WebDriver initDriver(Properties prop) // call by reference
 	{
 		String bowserName = prop.getProperty("browser").trim();
-		
-		System.out.println("Browser name is : "+ bowserName);
-		
+
+		System.out.println("Browser name is : " + bowserName);
+
 		switch (bowserName.toLowerCase()) {
 		case "chrome":
 			driver = new ChromeDriver();
@@ -32,33 +32,67 @@ public class DriverFactory {
 		case "firefox":
 			driver = new FirefoxDriver();
 			break;
-		
+
 		default:
 			System.out.println("Please pass the right browser...." + bowserName);
 			throw new FrameException("NOBROWSERFOUNDEXCEPTION");
-	}
-		
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.get(prop.getProperty("url"));
 		return driver;
 	}
-	
+
 	public Properties initProperties() {
-		
+
 		Properties prop = new Properties();
+		FileInputStream ip = null;
+
+		String envName = System.getProperty("env");
+		System.out.println("Runing test case on: " + envName);
 		try {
-			FileInputStream ip = new FileInputStream("./src/main/resources/config/config.properties");
-			prop.load(ip);
+			if (envName == null) {
+				System.out.println("No env is given hence running on config env..env.");
+
+				ip = new FileInputStream("./src/main/resources/config/config.properties");
+
+			} else {
+				switch (envName.toLowerCase().trim()) {
+				case "qa":
+					ip = new FileInputStream("./src/main/resources/config/qa.config.properties");
+					break;
+
+				case "uat":
+					ip = new FileInputStream("./src/main/resources/config/uat.config.properties");
+					break;
+
+				default:
+					System.out.println("Please pass the right env: " + envName);
+
+				}
+
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		}
+
+//		try {
+//			ip = new FileInputStream("./src/main/resources/config/config.properties");
+//			prop.load(ip);
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		try {
+			prop.load(ip);
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		return prop;
-		
-		
+
 	}
 
 }
