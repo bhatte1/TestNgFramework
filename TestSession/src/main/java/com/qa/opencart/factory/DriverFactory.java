27082c42d51f8ef3bce20392c9453c05826a8b8b
bhatte1 <vinayak.bhatte@gmail.com>
2023-08-10 23:15:46 -0400

    screenshots with extent reports

--- TestSession/src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ TestSession/src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -1,10 +1,14 @@
 package com.qa.opencart.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -16,6 +20,13 @@ public class DriverFactory {
 
 	WebDriver driver;
 	OptionsManager om;
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
+	
+	// return the thread local copy of the driver
+	public synchronized static WebDriver getDriver() {
+		return tlDriver.get();
+	}
+
 
 	public WebDriver initDriver(Properties prop) // call by reference
 	{
@@ -27,7 +38,7 @@ public class DriverFactory {
 		switch (bowserName.toLowerCase()) {
 		case "chrome":
 			driver = new ChromeDriver(om.getChromeOptions());
-			break;
+			break; 
 		case "edge":
 			driver = new EdgeDriver(om.getEdgeOptions());
 			break;
@@ -96,5 +107,23 @@ public class DriverFactory {
 		return prop;
 
 	}
+	
+	/**
+	 * take screenshot
+	 */
+	public static String getScreenshot() {
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+
+		try {
+			FileUtils.copyFile(srcFile, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return path;
+	}
+
 
 }
